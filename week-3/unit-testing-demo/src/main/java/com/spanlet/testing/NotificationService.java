477cf8c896054ba0db2b7599a2924793668a788b
Jiten <jpalaparthi@gmail.com>
2026-07-29 17:29:17 +0530

    add more on unit tests

--- week-3/unit-testing-demo/src/main/java/com/spanlet/testing/NotificationService.java
+++ week-3/unit-testing-demo/src/main/java/com/spanlet/testing/NotificationService.java
@@ -3,3 +3,11 @@ package com.spanlet.testing;
 public interface NotificationService {
     void sendWelcomeEmail(User user);
 }
+
+// public class DummyNotification extends NotificationService{
+
+// public void sendWelcomeEmail(User user){
+
+// }
+
+// }
